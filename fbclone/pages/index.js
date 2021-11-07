cbbf72308b1720e5790a5f5a6d98cb870bb9668f
pendimadyana <pendimadyana@gmail.com>
2021-11-07 14:49:04 +0700

    add fb auth using nextauth

--- fbclone/pages/index.js
+++ fbclone/pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
+import { getSession } from 'next-auth/client';
 
-export default function Home() {
+export default function Home({session}) {
+
+  if(!session) return <Login/>;
+
   return (
     <div>
       <Head>
@@ -18,4 +23,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session=await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
