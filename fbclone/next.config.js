914f1f30f36eb7aacbc63b1116d824797c3e7edc
pendimadyana <pendimadyana@gmail.com>
2021-11-07 09:36:25 +0700

    header setup

---
+++ fbclone/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: [
+            "links.papareact.com",
+            "platform-lookaside.fbsbxd.com",
+            "firebasestorage.googleapis.com",
+        ],
+    },
+};
